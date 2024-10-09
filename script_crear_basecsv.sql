4ed6f6e388078f2cb8ff44b989bc5bf833adfe84
Meli <mpiacentino@itba.edu.ar>
2024-10-09 12:37:39 -0300

    agregar scripts de csv

---
+++ script_crear_basecsv.sql
@@ -0,0 +1,7 @@
+\COPY public.consultas TO 'C:/Users/Meli/OneDrive/Documentos/ITBA/InfoMed/MedicalCenter/consultas.csv' DELIMITER ';' CSV HEADER NULL 'NULL';
+\COPY public.especialidades TO 'C:/Users/Meli/OneDrive/Documentos/ITBA/InfoMed/MedicalCenter/especialidades.csv' DELIMITER ';' CSV HEADER NULL 'NULL';
+\COPY public.medicamentos TO 'C:/Users/Meli/OneDrive/Documentos/ITBA/InfoMed/MedicalCenter/medicamentos.csv' DELIMITER ';' CSV HEADER NULL 'NULL';
+\COPY public.medicos TO 'C:/Users/Meli/OneDrive/Documentos/ITBA/InfoMed/MedicalCenter/medicos.csv' DELIMITER ';' CSV HEADER NULL 'NULL';
+\COPY public.pacientes TO 'C:/Users/Meli/OneDrive/Documentos/ITBA/InfoMed/MedicalCenter/pacientes.csv' DELIMITER ';' CSV HEADER NULL 'NULL';
+\COPY public.recetas TO 'C:/Users/Meli/OneDrive/Documentos/ITBA/InfoMed/MedicalCenter/recetas.csv' DELIMITER ';' CSV HEADER NULL 'NULL';
+\COPY public.sexobiologico TO 'C:/Users/Meli/OneDrive/Documentos/ITBA/InfoMed/MedicalCenter/sexobiologico.csv' DELIMITER ';' CSV HEADER NULL 'NULL';
